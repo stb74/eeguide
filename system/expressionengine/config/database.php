8929551758e052dd8f6c38a0f5987a6381118229
stb74 <scott.boyde@nifootball.co.uk>
2015-08-27 09:11:24 +0100

    Initial Commit

---
+++ system/expressionengine/config/database.php
@@ -0,0 +1,24 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_group = 'expressionengine';
+$active_record = TRUE;
+
+$db['expressionengine']['hostname'] = 'localhost';
+$db['expressionengine']['username'] = 'nifootball';
+$db['expressionengine']['password'] = 'etwbq2';
+$db['expressionengine']['database'] = 'eeguide';
+$db['expressionengine']['dbdriver'] = 'mysql';
+$db['expressionengine']['pconnect'] = FALSE;
+$db['expressionengine']['dbprefix'] = 'exp_';
+$db['expressionengine']['swap_pre'] = 'exp_';
+$db['expressionengine']['db_debug'] = TRUE;
+$db['expressionengine']['cache_on'] = FALSE;
+$db['expressionengine']['autoinit'] = FALSE;
+$db['expressionengine']['char_set'] = 'utf8';
+$db['expressionengine']['dbcollat'] = 'utf8_general_ci';
+$db['expressionengine']['cachedir'] = 'C:\\Users\\Scott\\OneDrive\\Sites\\eeguide.com\\_site\\system/expressionengine/cache/db_cache/';
+
+/* End of file database.php */
+/* Location: ./system/expressionengine/config/database.php */
+
+include(APPPATH."../../app/config/env_database.php");
